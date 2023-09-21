b325084d8d83059645cb42f14ab23804de66c5bf
iFission <alexwwj@icloud.com>
2023-09-21 13:53:03 +0800

    feat: add mason-null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,8 +1,15 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
+  dependencies = {
+    {
+      "jay-babu/mason-null-ls.nvim",
+      cmd = { "NullLsInstall", "NullLsUninstall" },
+      opts = { handlers = {} },
+    },
+  },
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+    local null_ls = require("null-ls")
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting

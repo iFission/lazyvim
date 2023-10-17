581fc478f55ac3057199fb2ead801d952395687b
iFission <alexwwj@icloud.com>
2023-10-17 13:31:32 +0800

    feat: upgrade lazyvim

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,5 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   dependencies = {
     {
       "jay-babu/mason-null-ls.nvim",
--- lua/plugins/nvim-navic.lua
+++ lua/plugins/nvim-navic.lua
@@ -4,7 +4,7 @@ return {
     lazy = true,
     init = function()
       vim.g.navic_silence = true
-      require("lazyvim.util").on_attach(function(client, buffer)
+      require("lazyvim.util").lsp.on_attach(function(client, buffer)
         if client.server_capabilities.documentSymbolProvider then
           require("nvim-navic").attach(client, buffer)
         end
@@ -19,5 +19,4 @@ return {
       }
     end,
   },
-
 }

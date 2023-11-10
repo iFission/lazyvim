18b423c3b902fbfec8f4ba2cd58f344f28a51bbb
iFission <alexwwj@icloud.com>
2023-11-10 11:58:56 +0800

    fix: revert to ts textsubjects

--- lua/plugins/nvim-treesitter-textsubjects.lua
+++ lua/plugins/nvim-treesitter-textsubjects.lua
@@ -6,9 +6,9 @@ return {
       require("nvim-treesitter.configs").setup({
         textsubjects = {
           enable = true,
-          -- prev_selection = ",", -- (Optional) keymap to select the previous selection
+          prev_selection = ",", -- (Optional) keymap to select the previous selection
           keymaps = {
-            -- ["."] = "textsubjects-smart",
+            ["."] = "textsubjects-smart",
             [";"] = "textsubjects-container-outer",
             ["i:"] = "textsubjects-container-inner",
           },
--- lua/plugins/wildfire-nvim.lua
+++ lua/plugins/wildfire-nvim.lua
@@ -1,6 +1,7 @@
 return {
   {
     "sustech-data/wildfire.nvim",
+    enabled = false,
     event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()

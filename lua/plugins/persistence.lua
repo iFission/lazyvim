316b801db1267e372152a710a6220b093a9a891e
iFission <alexwwj@icloud.com>
2023-12-13 11:40:13 +0800

    feat: save barbar in session

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,14 +2,19 @@ return {
   {
     "folke/persistence.nvim",
     event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "globals" } },
+    config = function()
+      require("persistence").setup({
+        pre_save = function()
+          vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
+        end,
+      })
+    end,
     -- stylua: ignore,
     keys = {
       { "<leader>qs", false },
       { "<leader>ql", false },
-      { "<leader>qd", false }
+      { "<leader>qd", false },
     },
   },
-
-
 }

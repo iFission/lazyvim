ba55017cc24ee1b9b3a20e834469199eab8aaf77
iFission <alexwwj@icloud.com>
2023-12-13 11:24:38 +0800

    fix: remove diff keymap

--- lua/plugins/telescope-diff-nvim.lua
+++ lua/plugins/telescope-diff-nvim.lua
@@ -3,12 +3,7 @@ return {
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
   },
-  init = function() require("telescope").load_extension "diff" end,
-  keys = {
-    {
-      "<leader>D",
-      function() require("telescope").extensions.diff.diff_current { hidden = true } end,
-      { desc = "Compare with file" },
-    },
-  },
+  init = function()
+    require("telescope").load_extension("diff")
+  end,
 }

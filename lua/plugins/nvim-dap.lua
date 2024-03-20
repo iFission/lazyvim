dc9bbabb6dd69d487d5ee78b305e9656ec1d1e7d
iFission <alexwwj@icloud.com>
2024-03-20 14:30:35 +0800

    feat: upgrade lazyvim packages

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -3,6 +3,7 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       { "rcarriga/nvim-dap-ui" },
+      { "nvim-neotest/nvim-nio" },
       { "theHamsta/nvim-dap-virtual-text" },
       { "nvim-telescope/telescope-dap.nvim" },
       { "jay-babu/mason-nvim-dap.nvim" },

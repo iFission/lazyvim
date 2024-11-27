bdd41e908fdc4c46d7493b37f8530f2b5e2e9c51
iFission <alexwwj@icloud.com>
2024-11-27 12:22:05 +0800

    feat: add auto open readme if no session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,12 +1,22 @@
 return {
   {
-    'rmagatti/auto-session',
+    "rmagatti/auto-session",
     config = function()
-      require("auto-session").setup {
+      require("auto-session").setup({
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      }
+        no_restore_cmds = {
+          function()
+            vim.notify("No sessions, opening README")
+            local readme_path = vim.fn.getcwd() .. "/README.md"
+            if vim.fn.filereadable(readme_path) == 1 then
+              vim.cmd("edit " .. readme_path)
+            end
+          end,
+        },
+      })
     end,
+
     lazy = false,
   },
 }

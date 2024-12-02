c7d3aa70a14825616c0d103c4ca63feae4c0863d
iFission <alexwwj@icloud.com>
2024-12-02 14:31:48 +0800

    fix: check no session no filename

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -7,10 +7,12 @@ return {
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
         no_restore_cmds = {
           function()
-            vim.notify("No sessions, opening README")
-            local readme_path = vim.fn.getcwd() .. "/README.md"
-            if vim.fn.filereadable(readme_path) == 1 then
-              vim.cmd("edit " .. readme_path)
+            if vim.fn.getbufinfo(1)[1].name == "" and vim.bo[vim.fn.bufnr(1)].filetype == "" then
+              vim.notify("No sessions, opening README")
+              local readme_path = vim.fn.getcwd() .. "/README.md"
+              if vim.fn.filereadable(readme_path) == 1 then
+                vim.cmd("edit " .. readme_path)
+              end
             end
           end,
         },

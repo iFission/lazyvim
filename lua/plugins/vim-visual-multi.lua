c6b22e46d73996ffd7bda7431c4034b3a3f39189
iFission <alexwwj@icloud.com>
2025-11-19 11:28:51 +0800

    fix: add vm add next line cursor

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     vim.g.VM_default_mappings = 0
     vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(VM-Find-Under)", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-m>", "<Plug>(VM-Add-Cursor-Down)", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "gV", "<Plug>(VM-Reselect-Last)", { noremap = true, silent = true })
   end,
 }

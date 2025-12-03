e4721ada3570f109590558586b0d5efb084d6949
iFission <alexwwj@icloud.com>
2025-12-03 10:44:19 +0800

    fix: fix move

--- lua/plugins/move-nvim.lua
+++ lua/plugins/move-nvim.lua
@@ -2,6 +2,21 @@ return {
   {
     "fedepujol/move.nvim",
     event = "BufEnter",
-    opts = {},
+    opts = {
+      line = {
+        enable = true, -- Enables line movement
+        indent = true, -- Toggles indentation
+      },
+      block = {
+        enable = true, -- Enables block movement
+        indent = true, -- Toggles indentation
+      },
+      word = {
+        enable = true, -- Enables word movement
+      },
+      char = {
+        enable = true, -- Enables char movement
+      },
+    },
   },
 }
